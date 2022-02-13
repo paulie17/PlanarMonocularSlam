aa4cab6c1576ea0255c9e9ff15c4775bdb897189
paulie17 <paolo@LAPTOP-ROBMP1LT.localdomain>
2022-02-13 09:38:54 +0100

    dataset and buil

---
+++ build/executables/CMakeFiles/test_gnuplot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_gnuplot.dir/test_gnuplot.cpp.o"
+  "CMakeFiles/test_gnuplot.dir/test_gnuplot.cpp.o.d"
+  "test_gnuplot"
+  "test_gnuplot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_gnuplot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
